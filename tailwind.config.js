c3c1d0a7d95afe053a86fe6c160f582243a182a1
ulascan54 <ulascandemirbag@gmail.com>
2021-09-15 22:25:32 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode:['jit'],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        'green':'#1DB954',
+        'dark':'#121212',
+        'light':'#282828',
+        'lightest':'#B3B3B3',
+        'darkest':'#191414'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
